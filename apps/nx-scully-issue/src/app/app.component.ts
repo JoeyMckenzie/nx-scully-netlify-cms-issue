8334ba5374dfa48d77b22e07264d653f664741cf
Joey Mckenzie <joey.mckenzie27@gmail.com>
2021-04-07 09:55:28 -0700

    initial commit

---
+++ apps/nx-scully-issue/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-scully-issue-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'nx-scully-issue';
+}
